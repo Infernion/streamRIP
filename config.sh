7e33d5ef135b6683e6adbb64b31b69d6e554e16c
Serhii Khalymon <sergiykhalimon@gmail.com>
2019-10-17 20:49:01 +0300

    refactor: update live_* endpoint names for stream origin

--- config.sh
+++ config.sh
@@ -3,10 +3,10 @@
 # The RTMP ingest stream
 rtmpi="rtmp://127.0.0.1/live_origin/stream"
 # The RTMP fallback stream. Switch to it when main stream has stopped playing
-rtmpi_fallback="rtmp://127.0.0.1/live_origin_fallback/stream"
+rtmpi_fallback="rtmp://127.0.0.1/live_backup/stream"
 
 # The RTMP output stream
-rtmpe="rtmp://127.0.0.1/live_origin_backed_up/stream"
+rtmpe="rtmp://127.0.0.1/live_output/stream"
 
 
 ###############
